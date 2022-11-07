7b9d8e689c1295953f905f725ae1571abe0c7543
FernandaTrochez <fernandatrochez0195@gmail.com>
2022-11-07 12:07:15 -0500

    Initial commit

---
+++ Network/Data/NewUser.swift
@@ -0,0 +1,17 @@
+//
+//  NewUser.swift
+//  Networking
+//
+//  Created by Mafe Trochez on 2/11/2022.
+//
+
+import Foundation
+
+struct NewUser: Encodable {
+
+    let name: String?
+    let email: String?
+    let gender: String?
+    let status: String?
+
+}
